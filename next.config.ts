c1a2909fd71ef73fa1578ef94ffb4e1e76ccdb82
Mojib Rsm <mojibrsm@gmail.com>
2025-08-25 13:57:51 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase: Error (a

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 
 import type {NextConfig} from 'next';
+import { config } from 'dotenv';
+
+// Load environment variables from .env file
+config();
 
 const nextConfig: NextConfig = {
   /* config options here */
