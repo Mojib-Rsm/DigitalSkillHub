c9038bf26066dbeebff98c4b00d31099f3c5a385
Mojib Rsm <mojibrsm@gmail.com>
2025-08-26 17:50:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 
 import type {NextConfig} from 'next';
-import { config } from 'dotenv';
-
-// Load environment variables from .env file
-config();
 
 const nextConfig: NextConfig = {
   /* config options here */
