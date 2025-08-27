3cc520c1b15fc268c097e5408d924db82b4c2c7f
Mojib Rsm <mojibrsm@gmail.com>
2025-08-27 07:02:15 +0000

    login charao sokol service use kora zabe

--- src/app/ai-tools/layout.tsx
+++ src/app/ai-tools/layout.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import ToolAuthGuard from "@/components/tool-auth-guard";
-
 export default function AiToolsLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return <ToolAuthGuard>{children}</ToolAuthGuard>;
+    return <>{children}</>;
 }
