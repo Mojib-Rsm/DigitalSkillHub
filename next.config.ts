a126cc3b2e2247f2141bf2f0050120394c2d3101
Mojib Rsm <mojibrsm@gmail.com>
2025-09-06 15:46:37 +0000

    ekhono eta asar karon ki?
    ## Error Type
    Console Error
    
    ## Error Message

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'ik.imagekit.io',
+      },
+      {
+        protocol: 'https',
         hostname: '**',
       },
       {
