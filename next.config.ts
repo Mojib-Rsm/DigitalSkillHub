6f17d7c7ecbc63aa85915b0f0dd12e8070cc577e
Mojib Rsm <mojibrsm@gmail.com>
2025-09-06 18:33:21 +0000

    Try fixing this error: `Console Error: Invalid src prop (https://ik.imag

--- next.config.ts
+++ next.config.ts
@@ -18,21 +18,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'ik.imagekit.io',
-        port: '',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '**',
+        hostname: '**',
       },
     ],
   },
