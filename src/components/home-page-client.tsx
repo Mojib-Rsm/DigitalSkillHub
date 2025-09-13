bc5630e052698c04098c453c9011f4efcfdda6f2
Mojib Rsm <mojibrsm@gmail.com>
2025-09-13 11:04:09 +0000

    eta zaccheina, zehetu eta zacchena, tai ekhane sthayi ba permanent solve

--- src/components/home-page-client.tsx
+++ src/components/home-page-client.tsx
@@ -228,11 +228,10 @@ export default function HomePageClient({ pricingPlans, testimonials, trendingToo
                                     </div>
                                 </div>
                                 <div className="relative w-full h-64 md:h-96 animate-fade-in-up [animation-delay:200ms]">
-                                    <Image 
+                                    <img 
                                         src={item.image} 
                                         alt={item.title} 
-                                        fill
-                                        className="object-cover rounded-lg shadow-xl"
+                                        className="object-cover w-full h-full rounded-lg shadow-xl"
                                         data-ai-hint={item.dataAiHint}
                                     />
                                 </div>
@@ -609,5 +608,7 @@ export default function HomePageClient({ pricingPlans, testimonials, trendingToo
   );
 
 }
+
+    
 
     
