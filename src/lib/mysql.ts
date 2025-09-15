4df4221bd12d782fae0b13e1139fbdbe205f21f3
Mojib Rsm <mojibrsm@gmail.com>
2025-09-15 13:25:04 +0000

    ?

--- src/lib/mysql.ts
+++ src/lib/mysql.ts
@@ -6,6 +6,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASS || "",
   database: process.env.DB_NAME || "totthoai",
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+  ssl: {
+    rejectUnauthorized: true,
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
