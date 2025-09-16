9b70f90744fb5c2f29b330f6e8003ff7f5aa7aa7
Mojib Rsm <mojibrsm@gmail.com>
2025-09-16 06:57:33 +0000

    Try fixing this error: `Console Error: getaddrinfo ENOTFOUND dpg-d34g50v

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,14 +2,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: process.env.POSTGRES_URL,
 });
 
 export default pool;
