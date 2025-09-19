475c36d51f0d02994b93027bd0cb666c32d970d6
Mojib Rsm <mojibrsm@gmail.com>
2025-09-19 11:55:17 +0000

    Internal Server Error

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,6 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({apiKey: process.env.GEMINI_API_KEY}),
-    googleAI({apiKey: process.env.GEMINI_API_KEY_2}),
   ],
   model: 'googleai/gemini-1.5-flash-latest',
 });
