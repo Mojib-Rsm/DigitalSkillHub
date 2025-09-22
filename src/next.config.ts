90d02ad19efd9a7889a4ade7d189e198f2bf7f71
Mojib Rsm <mojibrsm@gmail.com>
2025-09-22 12:09:15 +0000

    The Module not found: Can't resolve 'handlebars/dist/cjs/handlebars.js'

--- src/next.config.ts
+++ src/next.config.ts
@@ -3,9 +3,17 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   typescript: {
-    ignoreBuildErrors: true, 
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
   eslint: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    // !! WARN !!
     ignoreDuringBuilds: true,
   },
   images: {
